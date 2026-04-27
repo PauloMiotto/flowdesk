433662e0c4c149fe1b37d26e63e9f2567835520c
Paulo <pauloernestomiotto@gmail.com>
2026-04-27 18:38:00 -0400

    feat: allow public access to health endpoint

--- src/main/java/com/paulomiotto/flowdesk/health/HealthController.java
+++ src/main/java/com/paulomiotto/flowdesk/health/HealthController.java
@@ -2,11 +2,20 @@ package com.paulomiotto.flowdesk.health;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 @RestController
 public class HealthController {
     @GetMapping("/health")
     public String health() {
-        return "OKAY!";
+
+        LocalDateTime agora = LocalDateTime.now();
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+        String dataFormatada = agora.format(formatador);
+
+        return "OKAY - " + dataFormatada;
     }
 }
